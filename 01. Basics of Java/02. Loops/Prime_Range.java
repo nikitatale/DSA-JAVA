de1c34f54a23dacdb19a23cc86b71ae26cb5b103
nikitatale <talenikita3@gmail.com>
2026-01-24 23:48:17 +0530

    Optimize prime check using square root logic

--- 01. Basics of Java/02. Loops/Prime_Range.java
+++ 01. Basics of Java/02. Loops/Prime_Range.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class Prime_Range{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.err.print("Enter number: ");
         int n = sc.nextInt();
 
         for (int num = 2; num <= n; num++) {
             boolean isPrime = true;
 
-            for (int i = 2; i <= num / 2; i++) {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) {
                     isPrime = false;
                     break;
