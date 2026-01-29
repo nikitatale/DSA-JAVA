a775f5c1db90ef3b733ef5aa7567789c6e194efd
nikitatale <talenikita3@gmail.com>
2026-01-29 23:56:05 +0530

    add program to find the largest number in an array

---
+++ 02 Arrays/1. Easy/LargestNum.java
@@ -0,0 +1,27 @@
+// Total Time Complexity
+// O(n)
+//because we have to check every element to find the largest.
+
+// Space Complexity
+// We only use one extra variable largest, regardless of array size.
+// O(1)  (constant space)
+
+
+public class LargestNum{
+
+    public static int largestNum(int numbers[]){
+       int largest = Integer.MIN_VALUE;
+       
+       for(int i=0; i<numbers.length; i++){
+        if(largest < numbers[i]){
+            largest = numbers[i];
+        }
+       }
+       return largest;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {10, 22, 76, 88, 45};
+        System.out.println("Largest Number = " + largestNum(numbers));
+    }
+}
