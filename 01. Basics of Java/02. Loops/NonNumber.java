0a4c1569c1c5268f0bb7e68055e6b81ca9e786ed
Nikita Tale <talenikita3@gmail.com>
2026-02-09 18:56:09 +0530

    explained the logical steps

--- 01. Basics of Java/02. Loops/NonNumber.java
+++ 01. Basics of Java/02. Loops/NonNumber.java
@@ -1,6 +1,12 @@
+// ASCII value of A is 65 
+// @ is 64
+// so A - @ = 1
+// e = 101
+// loop is starting from 1 to 101
+
 public class NonNumber{
     public static void main(String[] args){
-        for(int i='A'-'@'; i<'e'; i++){
+        for(int i='A'-'@'; i <'e'; i++){
             System.out.println(i);
         }
     }
