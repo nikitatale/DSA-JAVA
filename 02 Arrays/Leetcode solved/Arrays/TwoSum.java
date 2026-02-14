9e640fafb1b51220a322f63325f3e80253af6930
Nikita Tale <talenikita3@gmail.com>
2026-02-14 23:27:16 +0530

    folder structure changed with new seperation of problems

--- 02 Arrays/Leetcode solved/Arrays/TwoSum.java
+++ 02 Arrays/Leetcode solved/Arrays/TwoSum.java
@@ -6,6 +6,12 @@
 // Break used correctly
 // O(n) solution
 
+// Since the array is sorted, I used the two-pointer technique.
+// I initialized one pointer at the beginning and one at the end.
+// If the sum was smaller than the target, I moved the left pointer forward to increase the sum.
+// If the sum was larger, I moved the right pointer backward to decrease it.
+// Once I found the target sum, I returned the 1-based indices as required.
+
 
 import java.util.Arrays;
 
@@ -17,7 +23,7 @@ public class TwoSum{
     while (start <= end) {
       int sum = numbers[start] + numbers[end];  
       if(sum == target){
-        arr[0] = start+1;
+        arr[0] = start+1; //by adding +1 we can achieve 1-based indexing
         arr[1] = end+1;
         break;
       } else if(sum < target){
