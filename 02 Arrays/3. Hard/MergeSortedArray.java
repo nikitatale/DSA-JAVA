da8bb89e9267a8b0a9dfd47eea82afbbd9df9b32
Nikita Tale <talenikita3@gmail.com>
2026-02-26 22:54:52 +0530

    Solve Merge sorted array without using extra space in O(m+n) time complexity

---
+++ 02 Arrays/3. Hard/MergeSortedArray.java
@@ -0,0 +1,32 @@
+// Time Complexity - O(m+n) & Space Complexity - O(1)
+
+
+import java.util.Arrays;
+
+public class MergeSortedArray{
+
+     public static  void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx = (m+n-1), i=m-1, j=n-1;
+
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[idx--] = nums1[i--];
+            } else {
+                nums1[idx--] = nums2[j--];
+            }
+        }
+
+        while(j >= 0){
+            nums1[idx--] = nums2[j--];
+        }
+    }
+
+     public static void main(String[] args){
+        int nums1[] = {1,2,3,0,0,0};
+        int nums2[] = {2,5,6};
+        int n = 3;
+        int m = 3;
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
