e7b8d79a9c53052991005c55b8a96b9112ce97d0
Nikita Tale <talenikita3@gmail.com>
2026-03-02 23:24:50 +0530

    solved MostFrequentEvenElement leetcode problem

---
+++ 02 Arrays/Leetcode solved/Arrays/MostFrequentEvenElement.java
@@ -0,0 +1,34 @@
+// An integer array nums, return the most frequent even element.
+// If there is a tie, return the smallest one. If there is no such element, return -1.
+// 
+
+
+public class MostFrequentEvenElement{
+        public static int mostFrequentEvenElement(int nums[]){
+        int ans = -1;
+        int maxCount = 0;
+        for(int i=0; i<nums.length; i++){
+           if(nums[i] % 2 == 0){
+               int count = 1;
+              for(int j=i+1; j<nums.length; j++){
+                  if(nums[i] == nums[j]){
+                      count++;
+                  }
+              }
+               if(count > maxCount){
+                   maxCount = count;
+                   ans = nums[i];
+               } else if(count == maxCount){
+                   ans = Math.min(ans, nums[i]);
+               }
+           }
+       }
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int nums[] = {0,1,2,2,4,4,1};
+        int value = mostFrequentEvenElement(nums);
+        System.out.print(value);
+    }
+}
