ca8b6def68ca363fa252a7482b70092ec1f558ea
Nikita Tale <talenikita3@gmail.com>
2026-03-19 23:47:22 +0530

    Removing all occurrences of given substring

---
+++ 03. Strings/1. Easy/RemoveOcc.java
@@ -0,0 +1,16 @@
+public class RemoveOcc{
+    public static String removeOccurrences(String s, String part) {
+         while(s.contains(part)){
+            int idx = s.indexOf(part);
+            s = s.substring(0, idx) + s.substring(idx + part.length());
+        }
+        return s; 
+    }
+
+    public static void main(String [] args){
+        String s = "daabcbaabcbc";
+        String part = "abc";
+        String res = removeOccurrences(s, part);
+        System.out.println(res);
+    }
+}
