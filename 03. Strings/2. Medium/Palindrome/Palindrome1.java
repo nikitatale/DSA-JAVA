4f0efa240255e881192e0a8b9b3170fc0f1ecd40
Nikita Tale <talenikita3@gmail.com>
2026-04-07 13:56:00 +0530

    Update folder structure + added one palindrome solution

---
+++ 03. Strings/2. Medium/Palindrome/Palindrome1.java
@@ -0,0 +1,19 @@
+public class Palindrome1{
+    
+    public static boolean isPalindrome(String str){
+      for(int i=0; i<str.length()/2; i++){
+        if(str.charAt(i) != str.charAt(str.length()-1-i)){
+            return false;
+        }
+      }
+
+      return true;
+    }
+
+    
+    public static void main(String[] args) {
+        String str = "noon";
+        boolean result = isPalindrome(str);
+        System.out.println(result);
+    }
+}
