81ae650b8c9c852403f7a7f7f62aa02b38826a66
Nikita Tale <talenikita3@gmail.com>
2026-04-10 23:35:52 +0530

    Solve leetcode medium level problem - Find minimum element in rotated sorted array

---
+++ 02 Arrays/2 Medium/MinSortedArray.java
@@ -0,0 +1,24 @@
+public class MinSortedArray{
+
+        public static  int findMin(int[] nums) {
+        int start = 0, end = nums.length-1;
+
+        while(start < end){
+           int mid = start + (end - start) / 2;
+
+             if(nums[mid] <= nums[end]){
+             end = mid;
+           } else {
+             start = mid + 1;
+           }
+        }
+
+        return nums[start];
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {3,4,5,1,2};
+        int min = findMin(nums);
+        System.out.println("Minimum element = " + min);
+    }
+}
